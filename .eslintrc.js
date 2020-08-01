eec1a16c72f90497811518d14c98407d3444ec0b
nokazn <nokazn@gmail.com>
2020-08-01 21:15:02 +0900

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   env: {
     es6: true,
-    node: true
+    node: true,
   },
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
@@ -12,29 +12,29 @@ module.exports = {
   ],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
+    SharedArrayBuffer: 'readonly',
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module'
+    sourceType: 'module',
   },
   plugins: [
-    '@typescript-eslint'
+    '@typescript-eslint',
   ],
-  'rules': {
+  rules: {
     'import/extensions': [2, {
-      'ts': 'never',
-      'tsx': 'never',
-      'js': 'never',
-      'jsx': 'never',
-      'json': 'never'
+      ts: 'never',
+      tsx: 'never',
+      js: 'never',
+      jsx: 'never',
+      json: 'never',
     }],
-    'indent': [2, 2],
+    indent: [2, 2],
     'lines-between-class-members': 0,
     'no-console': 0,
     // typescript-eslint の no-unuserd-vars を有効にする
     'no-unused-vars': 0,
     '@typescript-eslint/no-unused-vars': 2,
-  }
+  },
 };
